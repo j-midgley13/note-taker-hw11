4050c404a0e8d472186ff5c61b9657ba62754046
Jameson Midgley <jameson.midgley@gmail.com>
2020-08-12 14:34:30 -0600

    finally got POST to work. delete next.

--- Develop/server.js
+++ Develop/server.js
@@ -13,19 +13,56 @@ app.use(express.static('public'));
 
 // paths
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+app.get("/api/notes", (req, res) => {
+    fs.readFile('db/db.json', "utf8", function (error, data){
+        if (error){
+          return  console.log(error)
+        }
+
+        const newData = JSON.parse(data);
+
+        res.json(newData);
+    })
+    
 });
+
+// post path reads the json file, takes db.json into array, push aray, and rewrites db.json
+app.post("/api/notes", function(req, res) {        
+    
+    fs.readFile('db/db.json', "utf8", function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+
+      let jsonNotes = JSON.parse(data);
+
+      console.log(data);
+
+      jsonNotes.push(req.body);
+
+      fs.writeFile('db/db.json', JSON.stringify(jsonNotes), function(err, res){
+        if (err) {
+          return console.log(err);
+        }
+        return res;
+      })
+  })
+});
+
+// learned that * needs to come after the other paths.
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 
 //  listener
 app.listen(PORT, function() {
     console.log("Listening on PORT " + PORT)
 });
- 
